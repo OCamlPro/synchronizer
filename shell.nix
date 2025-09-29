62851943d366272165a83ba66aaa5a13e5c89058
zapashcanon <leo@ndrs.fr>
2025-09-29 11:38:04 +0200

    first commit

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  }) {}
+}:
+
+pkgs.mkShell {
+  name = "synchronizer-dev-shell";
+  dontDetectOcamlConflicts = true;
+  nativeBuildInputs = with pkgs.ocamlPackages; [
+    dune_3
+    findlib
+    ocaml
+    ocamlformat
+    odoc
+  ];
+  buildInputs = with pkgs.ocamlPackages; [
+    prelude
+  ];
+}
