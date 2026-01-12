7b0377b9ef88f92f2e4e78a674bb5e2d38940a83
redianthus <leo@ocaml.pro>
2026-01-12 11:18:45 +0100

    safer version

--- src/synchronizer.ml
+++ src/synchronizer.ml
@@ -5,8 +5,8 @@ type ('get, 'write) t =
   ; cond : Condition.t
   ; getter : unit -> 'get option
   ; writer : 'write -> unit
-  ; pledges_count : int Atomic.t
-  ; closed : bool Atomic.t
+  ; pledges_count : int ref
+  ; closed : bool ref
   }
 
 let init getter writer =
@@ -14,40 +14,32 @@ let init getter writer =
   ; cond = Condition.create ()
   ; getter
   ; writer
-  ; pledges_count = Atomic.make 0
-  ; closed = Atomic.make_contended false
+  ; pledges_count = ref 0
+  ; closed = ref false
   }
 
-let new_pledge synchro = Atomic.incr synchro.pledges_count
+let new_pledge synchro =
+  Mutex.protect synchro.mutex (fun () -> incr synchro.pledges_count)
 
 let end_pledge synchro =
-  let previous_pledges = Atomic.fetch_and_add synchro.pledges_count ~-1 in
-  (* it means the current pledges is 0 because we removed 1. *)
-  if previous_pledges = 1 then
-    (* we notify one thread that was waiting
-       it will take care of notifying other that there is nothing left to read *)
-    Condition.signal synchro.cond
+  Mutex.protect synchro.mutex (fun () ->
+    decr synchro.pledges_count;
+    if !(synchro.pledges_count) <= 0 then Condition.broadcast synchro.cond )
 
 let close synchro =
-  Atomic.set synchro.closed true;
-  (* The synchronizer is closed, we notify one thread only
-     it'll take care of notifying the others *)
-  Condition.signal synchro.cond
-
-let is_closed synchro = Atomic.get synchro.closed
+  Mutex.protect synchro.mutex (fun () ->
+    synchro.closed := true;
+    Condition.broadcast synchro.cond )
 
 let get ~pledge synchro =
   let rec inner_loop pledge synchro =
-    if is_closed synchro then None
+    if !(synchro.closed) then None
     else
       match synchro.getter () with
       | None ->
-        let pledges_count = Atomic.get synchro.pledges_count in
-        if pledges_count = 0 then begin
-          (* if someone else was waiting to read, it should try again now
-             we do not broadcast, because the next one is quickly going to notice
-             there is nothing left to read and notify the next thread once it is done *)
-          Condition.signal synchro.cond;
+        if !(synchro.pledges_count) = 0 then begin
+          (* if someone else was waiting to read, it should try again now *)
+          Condition.broadcast synchro.cond;
           None
         end
         else begin
@@ -58,16 +50,17 @@ let get ~pledge synchro =
           inner_loop pledge synchro
         end
       | next_element ->
-        if pledge then new_pledge synchro;
+        if pledge then incr synchro.pledges_count;
         next_element
   in
   Mutex.protect synchro.mutex (fun () -> inner_loop pledge synchro)
 
 let write synchro v =
-  if not @@ is_closed synchro then
-    Mutex.protect synchro.mutex (fun () ->
+  Mutex.protect synchro.mutex (fun () ->
+    if not !(synchro.closed) then begin
       synchro.writer v;
-      Condition.signal synchro.cond )
+      Condition.signal synchro.cond
+    end )
 
 let rec work_while f q =
   match get ~pledge:true q with
